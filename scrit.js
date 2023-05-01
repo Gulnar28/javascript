54983b3cb17eb78843ea60f342e230c5e98d736d
gulnarvma@outlook.com <gulnarvma@outlook.com>
2023-05-01 15:13:38 +0400

    commit

--- scrit.js
+++ scrit.js
@@ -1,53 +1,48 @@
 
-const fruitList=document.getElementById("fruitlist");
-const nameInput=document.getElementById("nameInput");
-const buttonAdd=document.getElementById("btn-add");
-const buttonCount=document.getElementById("btn-count");
-const buttonHide=document.getElementById("btn-hide");
 
 
-
-buttonAdd.addEventListener("click",showElement );
-buttonCount.addEventListener("click",fruitlist );
-buttonHide.addEventListener("click", hideElement);
-
-
-
-function countItems(listItem) {
-    let listItem = document.create("li");
-    listItem.classList.add("li")
-    listItem.innerText = "li";
-    fruitlist.appendChild(li);
-}
-
-
-   
-}
-function countFruit (){
-    var count = document.getElementsByTagName("li").length;
-    document.getElementById('count').innerHTML = 'Total used:' +count;
-   
-}
-
-
-newElement=document.getElementById('fruitlist');
-
-
-function hideshow() {
-    nameInput.classList.show('hidden');
-}
-if(newElement){
-    buttonAdd.classList.add("hide");
-}
-console.log(nameInput.classList);
-//     document.getElementById('fruitlist').style.height="100px";
-//     document.getElementById('fruitlist').style.display="block";
-//     document.getElementById('btn-hide').style.display="block";
-// }
-// function show() {
-//     document.getElementById('fruitlist').style.height="0px";
-//     document.getElementById('fruitlist').style.display="none";
-//     document.getElementById('btn-hide').style.display="inline";
-// }
-    
+//   EX1
   
+
+let inputelement=document.getElementById("inputelement");
+let list=document.getElementById("fruitlist");
+let btnAdd=document.getElementById("btnAdd");
+
+// EX1
+
+btnAdd.addEventListener('click',() =>{
+    const ul=divList.querySelector('ul');
+    const li=document.createElement('li');
+    li.innerHTML=inputelement.value;
+    inputelement.value='';
+    ul.appendChild(li);
+});
+
+
+//   EX2
+    function countListItems(){
+       let count = document.getElementsByTagName("li").length;
+        document.getElementById('demo').innerHTML = ` ${count}`;
+       
+    }
+   
+    // EX3
+
+    const toggleBtn=document.querySelector('#toggleBtn');
+    const divList=document.querySelector('#elementToWorkOn');
+    
+    toggleBtn.addEventListener('click',() =>{
+        if(divList.style.display==='none'){
+            divList.style.display='block';
+            toggleBtn.innerHTML='Hide List';
+        }
+        else{
+            divList.style.display='none'; 
+            toggleBtn.innerHTML='Show List';}
+
+        });
+    
+
+
+
+
