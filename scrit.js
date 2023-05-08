84053a4690b8141f793cd19417d9b753d7a32912
gulnarvma@outlook.com <gulnarvma@outlook.com>
2023-05-08 21:43:44 +0400

    first commit

--- scrit.js
+++ scrit.js
@@ -1,47 +1,81 @@
 
 
 
-//   EX1
+// // //   EX1-EX3;
   
 
-let inputelement=document.getElementById("inputelement");
-let list=document.getElementById("fruitlist");
-let btnAdd=document.getElementById("btnAdd");
-
-// EX1
-
-btnAdd.addEventListener('click',() =>{
-    const ul=divList.querySelector('ul');
-    const li=document.createElement('li');
-    li.innerHTML=inputelement.value;
-    inputelement.value='';
-    ul.appendChild(li);
-});
+// let inputelement=document.getElementById("inputelement");
+// let list=document.getElementById("fruitlist");
+// let btnAdd=document.getElementById("btnAdd");
 
 
-//   EX2
-    function countListItems(){
-       let count = document.getElementsByTagName("li").length;
-        document.getElementById('demo').innerHTML = ` ${count}`;
+
+// btnAdd.addEventListener('click',() =>{
+//     const ul=divList.querySelector('ul');
+//     const li=document.createElement('li');
+//     li.innerHTML=inputelement.value;
+//     inputelement.value='';
+//     ul.appendChild(li);
+// });
+
+
+
+//     function countListItems(){
+//        let count = document.getElementsByTagName("li").length;
+//         document.getElementById('demo').innerHTML = ` ${count}`;
        
-    }
+//     }
    
-    // EX3
-
-    const toggleBtn=document.querySelector('#toggleBtn');
-    const divList=document.querySelector('#elementToWorkOn');
     
-    toggleBtn.addEventListener('click',() =>{
-        if(divList.style.display==='none'){
-            divList.style.display='block';
-            toggleBtn.innerHTML='Hide List';
-        }
-        else{
-            divList.style.display='none'; 
-            toggleBtn.innerHTML='Show List';}
 
-        });
+//     const toggleBtn=document.querySelector('#toggleBtn');
+//     const divList=document.querySelector('#elementToWorkOn');
     
+//     toggleBtn.addEventListener('click',() =>{
+//         if(divList.style.display==='none'){
+//             divList.style.display='block';
+//             toggleBtn.innerHTML='Hide List';
+//         }
+//         else{
+//             divList.style.display='none'; 
+//             toggleBtn.innerHTML='Show List';}
+
+//         });
+
+
+
+        //EX4
+
+//         let countInput=document.getElementById('count');
+//         let generatorButton=document.getElementById('generator');
+//         let wrapper=document.getElementById('box');
+
+// function appendBoxText(i){
+    
+//     let listItem=document.createElement('div');
+//     listItem.classList.add('box');
+//     listItem.innerText=i;
+//     if (i%2===0)
+//     {
+//        listItem.classList.add('cut')
+//    }
+//    else{
+//     listItem.classList.add('tek')
+//    } 
+   
+//    wrapper.appendChild(listItem); 
+// }
+//         function handleBoxGeneration(count){
+//             for (let index = 1; index <= count; index++) {
+            
+//              appendBoxText(index);
+               
+//             } 
+            
+            
+//         }
+        
+//     generatorButton.addEventListener('click',()=>handleBoxGeneration(count.value));
 
 
 
